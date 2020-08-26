6377835e4b2ae3b191acc0ca35591ddaf5406944
Jordano-Jackson <eat333@naver.com>
2020-08-26 18:00:06 +0900

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,4 +3,11 @@ import time
 import datatime
 
 with picamera.Picamera() as camera:
+  camera.resolution = (640,480)
+  now = datatime.datatime.now()
+  filename = now.strftime('&Y-&m=&d &H:&M:%S')
+  camera.start_recording(output = filename + '.h264')
+  camera.wait_recording(5)
+  camera.stop_recording
+  
   
